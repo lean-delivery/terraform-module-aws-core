3df0166e570b55b9a413e23d0d74b53c310ddd88
Arman Zholmyrzayev <xat.keldime@gmail.com>
2019-02-28 20:28:34 +0600

    add name tag to NAT instance

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ resource "aws_instance" "nat" {
   vpc_security_group_ids = ["${aws_default_security_group.assign-name.id}"]
   subnet_id              = "${element(module.vpc.public_subnets, count.index)}"
 
-  tags = "${merge(local.default_tags, var.tags)}"
+  tags = "${merge(local.default_tags, var.tags, map("Name", "${var.project}-${var.environment}-${count.index}"))}"
 }
 
 resource "aws_route" "private_nat_ec2" {
