52ccd4cce67a1579155607a736d394f26805efda
Dmitry Gryaznukhin <identificator@gmail.com>
2019-10-23 10:22:39 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -59,6 +59,7 @@ module "vpc" {
   database_subnet_group_tags   = "${var.database_subnet_group_tags}"
 
   enable_nat_gateway      = "${ var.enable_nat_gateway ? "${ var.nat_as_ec2_instance ? "false" : "true" }" : "false" }"
+  enable_vpn_gateway      = "${ var.enable_vpn_gateway }"
   single_nat_gateway      = "${var.single_nat_gateway}"
   map_public_ip_on_launch = "${var.map_public_ip_on_launch}"                                                            // true by default
 
